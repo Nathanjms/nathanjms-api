a749c62a66264b0ccd63dbed5d95593cb0f6f5c1
Nathan James <nathanjms@outlook.com>
2021-11-30 20:18:38 +0000

    cors?

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.nathanjms.co.uk'],
+    'allowed_origins' => ['*.nathanjms.co.uk/*'],
 
     'allowed_origins_patterns' => [],
 
