e3aa18572aa0fa94d5d5a9f51173f247b0437293
Nathan James <nathanjms@outlook.com>
2021-11-30 20:14:05 +0000

    Route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::put('reset-password', [AuthController::class, 'resetPassword']);
 
 // Protected Routes
 Route::group(['middleware' => ['auth:sanctum']], function() {
