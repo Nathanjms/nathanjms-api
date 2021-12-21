3a4ebfb5480d60ec3d30a0850d3701416d8dab21
Nathan James <nathanjms@outlook.com>
2021-12-21 23:33:26 +0000

    Allow any port on localhost

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://*.nathanjms.co.uk', 'http://localhost:3000'],
+    'allowed_origins' => ['https://*.nathanjms.co.uk', 'http://localhost:*'],
 
     'allowed_origins_patterns' => [],
 
